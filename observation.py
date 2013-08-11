be5b9f980a59dcbb405a1e9b7fdd9905e281e502
W. Tyler Gee <wtylergee@gmail.com>
2013-08-10 21:41:18 -1000

    -Adding observation

---
+++ observation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import ephem
+
+class Session():
+    """Details about a particular observation session night"""
+    def __init__(self):
+        # initialize the iteration counter
+        self.date = ephem.now()
+
+if __name__=='__main__':
+    print("Nothing to see")
