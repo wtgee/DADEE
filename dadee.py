2327ca47270df4cdcf226914378df9453e1ca1e9
W. Tyler Gee <wtylergee@gmail.com>
2013-08-17 13:05:29 -1000

    -Adding session

--- dadee.py
+++ dadee.py
@@ -3,9 +3,9 @@
 import time
 import sys
 
-import Observation
+from observation import Session
 
 class DADEE():
     """Realtime plotting of Arduino light sensor data"""
     def __init__(self):
-        self.observation = ''
+        self.observation = Session()
